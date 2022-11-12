93b997568ecf0e1f2127cdc8b5adf7e0b44d39e8
joschi Pauschi <e1427350@student.tuwien.ac.at>
2022-11-12 20:28:22 +0100

    fix false bpm calculations

--- godot/signaler/signaler.gd
+++ godot/signaler/signaler.gd
@@ -1,6 +1,7 @@
 extends Node
 
 export(int) var bpm = 30
+var bps = bpm / 60.0
 
 func _ready():
 	var _timer_quarter_note = Timer.new()
@@ -11,20 +12,24 @@ func _ready():
 	add_child(_timer_new_tact)
 	
 	_timer_eighth_note.connect("timeout", self, "_on_eighth_note_timeout")
-	_timer_eighth_note.set_wait_time(bpm / 60.0)
+	_timer_eighth_note.set_wait_time(0.5 / bps)
 	_timer_eighth_note.set_one_shot(false)
 	
 	_timer_quarter_note.connect("timeout", self, "_on_quarter_note_timeout")
-	_timer_quarter_note.set_wait_time(bpm / 30.0)
+	_timer_quarter_note.set_wait_time(1.0 / bps)
 	_timer_quarter_note.set_one_shot(false)
 	
 	_timer_new_tact.connect("timeout", self, "_on_new_tact_timeout")
-	_timer_new_tact.set_wait_time(bpm / 7.5)
+	_timer_new_tact.set_wait_time(4.0 / bps)
 	_timer_new_tact.set_one_shot(false)
 	
 	_timer_quarter_note.start()
 	_timer_eighth_note.start()
 	_timer_new_tact.start()
+	
+	Events.emit_signal("quarter_note")
+	Events.emit_signal("eighth_note")
+	Events.emit_signal("new_tact")
 
 func _on_quarter_note_timeout():
 	Events.emit_signal("quarter_note")
