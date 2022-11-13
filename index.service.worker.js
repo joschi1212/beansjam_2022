6baed88050e27134e1e8d188150acdc75cd8a7ff
Plukers <Plukers@users.noreply.github.com>
2022-11-13 19:01:20 +0000

    Deploying to gh-pages from  @ 97947a67e918f94293bb2ce07a9c78f05a431c78 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1668365796|2455752";
+const CACHE_VERSION = "1668366059|2051049";
 const CACHE_PREFIX = "Game-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
