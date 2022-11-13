c917d5d45d3e68da3f1e399df90c4416fd690608
joschi1212 <joschi1212@users.noreply.github.com>
2022-11-13 16:12:06 +0000

    Deploying to gh-pages from  @ f030075cfbde6dbb90125293079b86f2848660d8 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1668353697|1395455";
+const CACHE_VERSION = "1668355903|2360994";
 const CACHE_PREFIX = "Game-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
